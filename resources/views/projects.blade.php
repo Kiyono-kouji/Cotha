128fcceeabf5ae1bf6a4081ffd5f257e2e20bd1f
Kenneth Jonathan Halim <kjonathan08@student.ciputra.ac.id>
2025-11-05 18:50:59 +0700

    Cleaned Projects Code and Added more Dummy data for the ProjectSeeder

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -38,31 +38,6 @@
                         </li>
                     @endif
 
-                    {{-- Pagination Elements --}}
-                    @foreach ($projects as $element)
-                        {{-- "Three Dots" Separator --}}
-                        @if (is_string($element))
-                            <li class="page-item disabled">
-                                <span class="page-link">{{ $element }}</span>
-                            </li>
-                        @endif
-
-                        {{-- Array Of Links --}}
-                        @if (is_array($element))
-                            @foreach ($element as $page => $url)
-                                @if ($page == $projects->currentPage())
-                                    <li class="page-item active">
-                                        <span class="page-link">{{ $page }}</span>
-                                    </li>
-                                @else
-                                    <li class="page-item">
-                                        <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                                    </li>
-                                @endif
-                            @endforeach
-                        @endif
-                    @endforeach
-
                     {{-- Next Page Link --}}
                     @if ($projects->hasMorePages())
                         <li class="page-item">
